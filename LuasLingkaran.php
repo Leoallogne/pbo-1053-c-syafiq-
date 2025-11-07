3548a78861c531f00eea4f6332003d6e6bfd44c9
Muhammad Syafiq <leo.allogne@gmail.com>
2025-11-08 01:05:26 +0700

    pertemuan3 v2

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,3 +1,13 @@
 <?php
-    echo "Hello World!";
-?>
+
+class LuasLingkaran {
+
+    public const phi = 3.14;
+    public int $jari;
+}
+
+$lingkaran = new LuasLingkaran();
+$lingkaran->jari = 12;
+
+$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
+echo "Hasilnya adalah: ".$rumus;
